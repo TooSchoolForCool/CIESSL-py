dd96c42c4f217f6b993e851a7d07d7821d3abef0
Zeyu Zhang <zeyuz@outlook.com>
2018-07-10 15:20:12 -0700

    replace queue with deque

--- ciessl/voice_engine/mic_array.py
+++ ciessl/voice_engine/mic_array.py
@@ -1,4 +1,4 @@
-import Queue
+import collections
 import threading
 import math
 
@@ -6,14 +6,19 @@ import pyaudio
 import numpy as np
 
 
+# Define some global variables here
+MAX_QUEUE_SIZE = 32
+
+
 class MicArray(object):
     
     def __init__(self, sample_rate=44100, n_channels=16, chunk_size=4096):
         self.pyaudio_ins_ = pyaudio.PyAudio()
 
-        self.queue_ = Queue.Queue()
+        self.queue_ = collections.deque(maxlen=MAX_QUEUE_SIZE)
 
         self.quit_event_ = threading.Event()
+        self.queue_cond_ = threading.Condition()
         
         self.n_channels_ = n_channels
         self.sample_rate_ = sample_rate
@@ -36,21 +41,35 @@ class MicArray(object):
 
 
     def start(self):
-        with self.queue_.mutex:
-            self.queue_.queue.clear()
+        if self.stream_.is_active():
+            return
 
+        self.queue_.clear()
         self.stream_.start_stream()
 
 
     def stop(self):
+        if self.stream_.is_stopped():
+            return
+
         self.quit_event_.set()
         self.stream_.stop_stream()
 
 
     def read_chunks(self):
+        if not self.stream_.is_active():
+            raise Exception("streaming is not started yet")
+
         self.quit_event_.clear()
         while not self.quit_event_.is_set():
-            frames = self.queue_.get()
+            self.queue_cond_.acquire()
+
+            if len(self.queue_) == 0:
+                self.queue_cond_.wait()
+
+            frames = self.queue_.popleft()
+
+            self.queue_cond_.release()
 
             # Cannot acquire signal from device
             if not frames:
@@ -61,7 +80,12 @@ class MicArray(object):
 
 
     def __callback(self, in_data, frame_count, time_info, status):
-        self.queue_.put(in_data)
+        self.queue_cond_.acquire()
+
+        self.queue_.append(in_data)
+        
+        self.queue_cond_.notify()
+        self.queue_cond_.release()
 
         # since we do NOT want to have any output, set the 
         # 1st return item to `None`
